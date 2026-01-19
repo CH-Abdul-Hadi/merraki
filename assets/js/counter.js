1b59175529b1966250b6022fddbaca311040065b
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2026-01-19 15:47:05 +0500

    feat: Add scroll-triggered animations and a counting effect, refactoring inline styles into dedicated CSS and JavaScript files.

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,14 +1,29 @@
-$(document).ready(function () {
-    $('.counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            
-            step: function (now) {
+document.addEventListener("DOMContentLoaded", function () {
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          let $this = $(entry.target);
+          $this.prop("Counter", 0).animate(
+            {
+              Counter: $this.text(),
+            },
+            {
+              duration: 4000,
+              easing: "swing",
+              step: function (now) {
                 $(this).text(Math.ceil(now));
+              },
             }
-        });
-    });
+          );
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  document.querySelectorAll(".counter").forEach((counter) => {
+    observer.observe(counter);
+  });
 });
